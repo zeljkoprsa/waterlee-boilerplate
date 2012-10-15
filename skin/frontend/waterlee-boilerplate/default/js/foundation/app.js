2a604e6a35db0ba1467b32ad802c756c95078eeb
Zeljko Prsa <zeljko.prsa@gmail.com>
2012-10-15 11:40:27 +0200

    Still Alpha! :: offCanvas current subcategories for mobile
    This is still and alpha version:
    - Added offCanvas
    - Added subcategories for mobile view
    - Still ugly, wait for Beta :)

--- skin/frontend/waterlee-boilerplate/default/js/foundation/app.js
+++ skin/frontend/waterlee-boilerplate/default/js/foundation/app.js
@@ -1,48 +1,25 @@
-;(function ($, window, undefined) {
-    'use strict';
+(function ($) {
 
-    var $doc = $(document),
-        Modernizr = window.Modernizr;
+    $(function(){
+        $(document).foundationTopBar();
+        $(document).foundationAlerts();
+        $(document).foundationButtons();
+        $(document).foundationAccordion();
+        $(document).foundationNavigation();
+        $(document).foundationCustomForms();
+        $(document).foundationMediaQueryViewer();
+        $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
 
-
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $('input, textarea').placeholder();
-
-
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-
-
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-
-
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-
-
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-
-
-    $.fn.foundationTabs             ? $doc.foundationTabs() : null;
+        $(document).tooltips();
+        $('input, textarea').placeholder();
 
 
 
-    $("#featured").orbit();
+        // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+        // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+        // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+        // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+        // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+    });
 
-
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
-    // Hide address bar on mobile devices
-    if (Modernizr.touch) {
-        $(window).load(function () {
-            setTimeout(function () {
-                window.scrollTo(0, 1);
-            }, 0);
-        });
-    }
-
-})(jQuery, this);
+})(jQuery);
