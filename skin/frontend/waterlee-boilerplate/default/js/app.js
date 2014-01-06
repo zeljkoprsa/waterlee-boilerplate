bc1783d56add242b07fd2df76ba4bbb2660ab9df
its4net <kris@scheppe.us>
2014-01-06 12:17:36 -0500

    Upgrade Waterloo to Foundation 5

--- skin/frontend/waterlee-boilerplate/default/js/app.js
+++ skin/frontend/waterlee-boilerplate/default/js/app.js
@@ -1,5 +1,3 @@
-// Initializes all JS functions from foundation.min.js
-jQuery(document).foundation();
-
-// Initialize specific functions when loading JSs separately. Comment out line 1 when doing that.
-//jQuery(document).foundation('joyride', 'start');
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
