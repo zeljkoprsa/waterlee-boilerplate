1ee4e379cbb9cb38c5d1f47ce3e6be9654cda7a6
marjanbonus <marjan.bonus@gmail.com>
2014-01-13 10:26:36 +0100

    Waterlee on Foundation 5

--- skin/frontend/waterlee-boilerplate/default/config.rb
+++ skin/frontend/waterlee-boilerplate/default/config.rb
@@ -1,6 +1,5 @@
-require 'zurb-foundation'
-# Require any additional compass plugins here.
 
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,7 +9,6 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
