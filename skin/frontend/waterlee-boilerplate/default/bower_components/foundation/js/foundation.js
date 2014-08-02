48cac333a7b4c2a7edd5c8d07d51487d1d914f65
marjanbonus <marjan.bonus@gmail.com>
2014-08-02 15:29:54 +0200

    Update to Foundation 5.3.3

--- skin/frontend/waterlee-boilerplate/default/bower_components/foundation/js/foundation.js
+++ skin/frontend/waterlee-boilerplate/default/bower_components/foundation/js/foundation.js
@@ -618,7 +618,7 @@
   Foundation.libs.abide = {
     name : 'abide',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       live_validate : true,
@@ -913,7 +913,7 @@
   Foundation.libs.accordion = {
     name : 'accordion',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       active_class: 'active',
@@ -979,7 +979,7 @@
   Foundation.libs.alert = {
     name : 'alert',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       callback: function (){}
@@ -1023,7 +1023,7 @@
   Foundation.libs.clearing = {
     name : 'clearing',
 
-    version: '5.3.2',
+    version: '5.3.3',
 
     settings : {
       templates : {
@@ -1582,7 +1582,7 @@
   Foundation.libs.dropdown = {
     name : 'dropdown',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       active_class: 'open',
@@ -1896,7 +1896,7 @@
   Foundation.libs.equalizer = {
     name : 'equalizer',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       use_tallest: true,
@@ -1971,7 +1971,7 @@
   Foundation.libs.interchange = {
     name : 'interchange',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     cache : {},
 
@@ -2318,7 +2318,7 @@
   Foundation.libs.joyride = {
     name : 'joyride',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     defaults : {
       expose                   : false,     // turn on or off the expose feature
@@ -3204,7 +3204,7 @@
   Foundation.libs['magellan-expedition'] = {
     name : 'magellan-expedition',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       active_class: 'active',
@@ -3394,7 +3394,7 @@
   Foundation.libs.offcanvas = {
     name : 'offcanvas',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       open_method: 'move',
@@ -3976,7 +3976,7 @@
   Foundation.libs.reveal = {
     name : 'reveal',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     locked : false,
 
@@ -4420,7 +4420,7 @@
   Foundation.libs.slider = {
     name : 'slider',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings: {
       start: 0,
@@ -4652,7 +4652,7 @@
   Foundation.libs.tab = {
     name : 'tab',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       active_class: 'active',
@@ -4820,7 +4820,7 @@
   Foundation.libs.tooltip = {
     name : 'tooltip',
 
-    version : '5.3.2',
+    version : '5.3.3',
 
     settings : {
       additional_inheritable_classes : [],
@@ -5119,7 +5119,7 @@
   Foundation.libs.topbar = {
     name : 'topbar',
 
-    version: '5.3.2',
+    version: '5.3.3',
 
     settings : {
       index : 0,
