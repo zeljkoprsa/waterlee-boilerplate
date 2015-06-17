ab459d9f6b14eba4bfb5521cfcbf17aef658cf3d
Marjan Bonus <marjan.bonus@gmail.com>
2015-06-17 22:51:01 +0200

    Templates & sass redesign - wip.

--- skin/frontend/waterlee-boilerplate/default/config.rb
+++ skin/frontend/waterlee-boilerplate/default/config.rb
@@ -4,7 +4,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
+sass_dir = "src/scss"
 images_dir = "images"
 javascripts_dir = "js"
 
--- skin/frontend/waterlee-boilerplate/default/src/js/app.js
+++ skin/frontend/waterlee-boilerplate/default/src/js/app.js
@@ -2,86 +2,123 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 jQuery(document).foundation();
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
 
-	// media query event handler
-if (matchMedia) {
-	var mq = window.matchMedia("(min-width: 640px)");
-	mq.addListener(WidthChange);
-	WidthChange(mq);
-}
+  //Flexslider
 
-// media query change
-function WidthChange(mq) {
+  jQuery(window).load(function() {
+    jQuery('.product-flexslider').flexslider({
+      animation: "slide",
+      slideshow: false,
+      maxItems: 2,
+      itemWidth: 50
+    });
+  });
 
-	if (mq.matches) {
-		// window width is at least 500px
-		  jQuery('.gallery-image.visible').elevateZoom();
-		  jQuery('.more-views').click(function(){
-		    jQuery('.gallery-image.visible').elevateZoom();
-		  })
+  // Product page / wishlist - quantity increase/decrease
+  jQuery(".quantity").append('<i id="add1" class="plus fa fa-plus" />').prepend('<i id="minus1" class="minus fa fa-minus" />');
+  jQuery(".quantity .plus").click(function(){
+    var currentVal = parseInt(jQuery(this).parent().find(".qty").val());
+    if (!currentVal || currentVal=="" || currentVal == "NaN") currentVal = 0;
+    jQuery(this).parent().find(".qty").val(currentVal + 1);
+  });
 
-	}
-	else {
-		// window width is less than 500px
-		 jQuery('.gallery-image.visible').elevateZoom({
-		    constrainType:"height",
-		    constrainSize:274,		   
-		    zoomType: "lens",
-		    containLensZoom: true,
-			cursor: "pointer",
-			galleryActiveClass: "active",
-			zoomWindowFadeIn: 500,
-			zoomWindowFadeOut: 750
-		    });
+  jQuery(".quantity .minus").click(function(){
+    var currentVal = parseInt(jQuery(this).parent().find(".qty").val());
+    if (currentVal == "NaN") currentVal = 0;
+    if (currentVal > 1){
+      jQuery(this).parent().find(".qty").val(currentVal - 1);
+    }
+  });
 
-		  jQuery('.more-views').click(function(){
-		    jQuery('.gallery-image.visible').elevateZoom({
-		    constrainType:"height",
-		    constrainSize:274,		   
-		    zoomType: "lens",
-		    containLensZoom: true,
-			cursor: "pointer",
-			galleryActiveClass: "active",
-			zoomWindowFadeIn: 500,
-			zoomWindowFadeOut: 750
-   			});
-		  })		
-	}
+  //Grid / List view
+  jQuery('.view-mode strong.grid').after('<i class="fa fa-th"></i>');
+  jQuery('.view-mode strong.list').after('<i class="fa fa-align-justify"></i>');
 
-}
- 	 jQuery('#demoTab').easyResponsiveTabs();
- 	 
- 	 //Scroll to the top
- 	 //Check to see if the window is top if not then display button
-	jQuery(window).scroll(function(){
-		if (jQuery(this).scrollTop() > 100) {
-			jQuery('.scrollToTop').fadeIn();
-		} else {
-			jQuery('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	jQuery('.scrollToTop').click(function(){
-		jQuery('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-
- 
-
-})
-
-	jQuery(window).load(function(){
-      jQuery('#carousel').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        itemWidth: 210,
-        itemMargin: 5,
-        asNavFor: '#slider'
-      });
+  jQuery('.view-mode a.list').each(function() {
+    if (jQuery(this).text() == 'List')
+      jQuery(this).text('');
+      jQuery(this).append('<i class="fa fa-align-justify"></i>');
     });
 
-	
+  jQuery('.view-mode a.grid').each(function() {
+    if (jQuery(this).text() == 'Grid')
+      jQuery(this).text('');
+      jQuery(this).append('<i class="fa fa-th"></i>');
+  });
+
+  //Top cart
+  jQuery(".top--cart").click(function(e) {
+    e.stopPropagation();
+    jQuery(this).toggleClass('active');
+  });
+  jQuery(document).click(function() {
+    jQuery('.top--cart').removeClass('active');    
+  });
+
+  //Scroll to top
+
+  jQuery('.footer-container').after('<div class="scrollToTop"></div>');
+  jQuery('.scrollToTop').append('<i class="fa fa-chevron-circle-up fa-2x"></i>');
+  jQuery(window).scroll(function(){
+    if (jQuery(this).scrollTop() > 100) {
+      jQuery('.scrollToTop').fadeIn();
+    } else {
+      jQuery('.scrollToTop').fadeOut();
+    }
+  });
+  
+  //Click event to scroll to top
+  jQuery('.scrollToTop').click(function(){
+    jQuery('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+
+
+  // media query event handler
+  if (matchMedia) {
+    var mq = window.matchMedia("(min-width: 640px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+  }
+
+  // media query change
+  function WidthChange(mq) {
+
+    if (mq.matches) {
+      // window width is at least 500px
+        jQuery('.gallery-image.visible').elevateZoom();
+        jQuery('.more-views').click(function(){
+          jQuery('.gallery-image.visible').elevateZoom();
+        })
+    }
+    else {
+      // window width is less than 500px
+       jQuery('.gallery-image.visible').elevateZoom({
+          constrainType:"height",
+          constrainSize:274,       
+          zoomType: "lens",
+          containLensZoom: true,
+        cursor: "pointer",
+        galleryActiveClass: "active",
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750
+          });
+
+        jQuery('.more-views').click(function(){
+          jQuery('.gallery-image.visible').elevateZoom({
+          constrainType:"height",
+          constrainSize:274,       
+          zoomType: "lens",
+          containLensZoom: true,
+        cursor: "pointer",
+        galleryActiveClass: "active",
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750
+          });
+        })    
+    }
+
+  }
+
+});
